fc06f1193e7fbf3d65bb589b554647b230a49887
German Casares <german.casares@outlook.com>
2022-01-17 19:32:34 -0800

    run migrations at startup

--- Program.cs
+++ Program.cs
@@ -93,11 +93,19 @@ builder.Services
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+} 
+else
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AlmanimeContext>();
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();

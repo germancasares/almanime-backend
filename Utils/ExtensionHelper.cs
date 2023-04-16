9e7fca2d327632b4a7ad15c3c5aa7bfd20a66686
Germán Casares <german.casares@outlook.com>
2023-04-16 20:40:25 +0000

    fix auth0ID is not guid

--- Utils/ExtensionHelper.cs
+++ Utils/ExtensionHelper.cs
@@ -10,7 +10,7 @@ public static class ExtensionHelper
   public static string GetAuth0ID(this ClaimsPrincipal user)
   {
     var auth0ID = user.GetAuth0IDOrDefault();
-    return auth0ID == null || !Guid.TryParse(auth0ID, out _) ? throw new AlmNullException(nameof(auth0ID)) : auth0ID;
+    return auth0ID ?? throw new AlmNullException(nameof(auth0ID));
   }
 
   public static string? GetAuth0IDOrDefault(this ClaimsPrincipal user) => user.FindFirst(ClaimTypes.NameIdentifier)?.Value;

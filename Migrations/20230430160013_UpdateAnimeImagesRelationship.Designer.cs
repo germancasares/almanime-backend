94c4c5ca3a86d70628b1d96bd3ca775919fabd39
Germán Casares <german.casares@outlook.com>
2023-04-30 16:00:44 +0000

    Update migrations

--- Migrations/20230430160013_UpdateAnimeImagesRelationship.Designer.cs
+++ Migrations/20230430160013_UpdateAnimeImagesRelationship.Designer.cs
@@ -4,6 +4,7 @@ using Almanime.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Almanime.Migrations
 {
     [DbContext(typeof(AlmanimeContext))]
-    partial class AlmanimeContextModelSnapshot : ModelSnapshot
+    [Migration("20230430160013_UpdateAnimeImagesRelationship")]
+    partial class UpdateAnimeImagesRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
